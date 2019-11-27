60f5f1710f8ccec98273536099a9ce288d58fcee
Mr <yuzhoujie95@gmail.com>
2019-11-27 16:37:56 +0800

    add typescript demo

---
+++ PC/ts/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ts demo</h1>
+        <p>A typescript demo file.For more information, see the console.</p>
+        <p>By: create-react-app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
